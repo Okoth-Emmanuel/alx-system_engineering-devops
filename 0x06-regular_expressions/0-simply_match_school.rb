6c519122c964e30f4c5480eab1b6e4c4b4b0e53b
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2024-05-07 03:27:36 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,12 @@
 #!/usr/bin/env ruby
-
-require 'oniguruma'
-
 # Get the argument passed to the script
 input = ARGV[0]
 
 # Define the regular expression pattern
-pattern = Oniguruma::ORegexp.new('School')
+pattern = /School/
 
 # Match the input against the pattern
-match = pattern.match(input)
+match = input.match(pattern)
 
 # Print the matched result
 puts match ? match[0] : ""
